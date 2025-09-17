d1f765c4f6b99fd7249bd4dcfe0cd9d601abaa49
rizwanakhtar01 <98576822+rizwanakhtar01@users.noreply.github.com>
2025-09-17 06:57:17 +0000

    fix deployment issue

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,14 @@
-'use client'
+"use client";
 
-import * as React from 'react'
+import * as React from "react";
 import {
   ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+  ThemeProviderProps,
+} from "next-themes";
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({
+  children,
+  ...props
+}: ThemeProviderProps & { children: React.ReactNode }) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
